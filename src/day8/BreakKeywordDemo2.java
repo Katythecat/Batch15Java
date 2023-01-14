c29b92581aa4e76a9e47aa27d300828dd899edca
Katythecat <kk.wilasinee@gmail.com>
2023-01-14 12:03:47 -0800

    Java class 8

---
+++ src/day8/BreakKeywordDemo2.java
@@ -0,0 +1,28 @@
+package day8;
+
+public class BreakKeywordDemo2 {
+    public static void main(String[] args) {
+
+
+        boolean summer = true;
+       int temp = 75;
+        while (summer) {
+            if (temp <= 100) {
+                System.out.println("I love summer because temperature is " + temp);
+            } else {
+                System.out.println("Its too hot "+temp);
+                summer = false;
+            }
+            temp += 5;
+        }
+
+        int tem;
+        for(tem=75; tem<=100; tem+=5) {
+            System.out.println("I love summer because temperature is "+tem);
+        }
+        System.out.println("Its very hot "+tem);
+
+    }
+
+    }
+
---
+++ src/day8/Replit63.java
@@ -0,0 +1,18 @@
+package day8;
+
+import java.util.Scanner;
+
+public class Replit63 {
+    public static void main(String[] args) {
+        Scanner input=new Scanner(System.in);
+        System.out.println("Please enter number");
+        int x=input.nextInt();
+        for(int i=0;i<x;i++){
+            System.out.print(i+ " ");
+        }
+
+        // System.out.println();
+    }
+
+
+}
